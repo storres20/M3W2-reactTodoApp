7c87a66fcba7a475440eed3fef59e6757e917398
storres20 <sanchotorres777@gmail.com>
2022-12-06 13:30:49 -0500

    Add About page

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from "react-router-dom"
+
+function About() {
+  return (
+    <div className="about__content">
+      <ul className="about__list">
+        <NavLink to="/about/about-app">About App</NavLink>
+        <NavLink to="/about/about-author">About Author</NavLink>
+      </ul>
+    </div>
+  )
+}
+
+export default About
